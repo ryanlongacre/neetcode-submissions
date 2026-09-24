ad34ac4878ed4153021613cf84dd0713a71e6689
ryanlongacre <155045055+ryanlongacre@users.noreply.github.com>
2026-09-24 14:00:03 -0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,39 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Trie()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Trie()
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+        
+        
